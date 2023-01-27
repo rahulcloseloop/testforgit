3f0ada3748c1a3950c7dcb631b171df64c148cd2
rahul <rahul.batra@closeloop.com>
2023-01-27 09:10:12 +0000

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!5");
+  res.send("Hello World!6");
 });
 
 const port = process.env.PORT || 3000;
